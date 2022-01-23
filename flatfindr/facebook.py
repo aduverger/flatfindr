a9af7795ce3d914eae8d5e007d95f40662f1f523
Alexandre Duverger <alexandre.duverger@protonmail.com>
2022-01-22 19:33:29 -0500

    scrape items links implemented for facebook.py

--- flatfindr/facebook.py
+++ flatfindr/facebook.py
@@ -1,23 +1,22 @@
 # facebook marketplace
+from email.policy import default
 import os
 import random
-from collections import defaultdict
-from io import BytesIO
 from time import sleep
 
-import requests
-from PIL import Image
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from collections import defaultdict
 
 from flatfindr.logins import LOGINS
+from flatfindr.utils import KEYWORDS
 
 
 class Facebook:
     def __init__(
         self,
-        email=LOGINS["facebook"][0],
-        password=LOGINS["facebook"][1],
+        email=LOGINS["facebook"]["id"],
+        password=LOGINS["facebook"]["pass"],
         path=os.path.join(
             os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "raw_data"
         ),
@@ -36,11 +35,26 @@ class Facebook:
             "prefs", {"profile.default_content_setting_values.notifications": 2}
         )
         self.driver = webdriver.Chrome(
-            options=option, executable_path="/Users/aduverger/Downloads/chromedriver",
+            options=option,
+            executable_path=os.path.join(
+                os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
+                "chromedriver",
+            ),
         )
 
         self.main_url = "https://www.facebook.com"
-        self.item_links = []
+        self.items_links = []
+        self.db = {  # TODO: importer db depuis un json
+            "url": [],
+            "published": [],
+            "price": [],
+            "address": [],
+            "surface": [],
+            "bedrooms": [],
+            "furnished": [],
+            "description": [],
+            "image": [],
+        }
 
     def log_in(self):
         self.driver.get(self.main_url)
@@ -62,8 +76,8 @@ class Facebook:
     def quit(self):
         self.driver.quit()
 
-    def scrape_item_links(
-        self, db=defaultdict(list), min_price=1_200, max_price=1_750, min_bedrooms=2
+    def scrape_items_links(
+        self, min_price=1_200, max_price=1_750, min_bedrooms=2, scroll=10
     ):
         url1 = "/marketplace/montreal/propertyrentals?"
         url2 = f"minPrice={min_price}&maxPrice={max_price}"
@@ -71,12 +85,12 @@ class Facebook:
         url4 = "&exact=false&latitude=45.5254&longitude=-73.5724&radius=2"
         self.driver.get(self.main_url + url1 + url2 + url3 + url4)
 
-        for i in range(10):
+        for _ in range(scroll):
             try:
                 self.driver.execute_script(
                     "window.scrollTo(0, document.body.scrollHeight);"
                 )
-                sleep(random.uniform(1, 1.5))
+                sleep(random.uniform(0.8, 1.2))
             except:
                 pass
 
@@ -85,83 +99,97 @@ class Facebook:
             item_url = item.get_attribute("href")
             if "/marketplace/item/" in item_url:
                 item_url = item_url[: item_url.find("?")]
-                if item_url not in db["url"]:
-                    self.item_links.append(item_url)
+                # If this item has never been scraped before, we add it
+                if item_url not in self.db["url"]:
+                    self.items_links.append(item_url)
 
-        return self.item_links
+        return self.items_links
 
-    def scrape_item_details(self, item_url, db=defaultdict(list)):
-        db["url"].append(item_url)
+    def scrape_item_details(self, item_url):
+        item_db = {}
+        item_db["url"] = item_url
 
-        fb.driver.get(item_url)
+        self.driver.get(item_url)
         # Click on 'see more' button to have the full description
-        buttons = fb.driver.find_elements_by_xpath("//div[@role='button']")
+        buttons = self.driver.find_elements_by_xpath("//div[@role='button']")
         _ = [button.click() for button in buttons if button.text == "Voir plus"]
         sleep(random.uniform(0.2, 0.5))
 
-        details = fb.driver.find_elements_by_xpath("//span[@dir]")
-        price, adress, surface, bedrooms, description = [True] * 5
+        details = self.driver.find_elements_by_xpath("//span[@dir]")
         for i in range(len(details)):
             detail = details[i].text
-            if price and "$ / mois" in detail:
-                db["price($/month)"].append(int(detail[:5].replace(" ", "")))
-                price = False
-            elif adress and "Montréal, QC" in detail:
-                db["adress"].append(detail.replace("Montréal, QC", ""))
-                adress = False
-            elif surface and "mètres carrés" in detail:
-                db["surface(m2)"].append(int(detail[:4].replace(" ", "")))
-                surface = False
-            elif surface and "pieds carrés" in detail:
-                db["surface(m2)"].append(
-                    round(int(detail[:4].replace(" ", "")) / 10.764)
+            if not item_db.get("published") and KEYWORDS["published"] in detail:
+                if KEYWORDS["day"] in detail:  # if published less than a week ago
+                    item_db["published"] = int(detail[20])
+                else:
+                    # if published more than a week ago, we use 8 as default
+                    item_db["published"] = 8
+            if not item_db.get("price") and KEYWORDS["price"] in detail:
+                item_db["price"] = int(detail[:5].replace(" ", ""))
+            elif not item_db.get("address") and KEYWORDS["address"] in detail:
+                item_db["address"] = detail.replace("Montréal, QC", "").replace(
+                    ", ", ""
                 )
-                surface = False
-            elif bedrooms and "chambres · " in detail:
-                db["bedrooms"].append(int(detail[0]))
-                bedrooms = False
-            elif detail in ("Meublé", "Non meublé"):
-                db["furnished"].append(detail)
-            elif description and detail == "Description":
-                db["description"].append(details[i + 1].text.replace("Voir moins", ""))
-                description = False
+            elif not item_db.get("surface") and KEYWORDS["surface(m2)"] in detail:
+                surface = int(detail[:4].replace(" ", ""))
+                # if the seller indicated square meters but is actually square feet
+                if surface > 200:
+                    surface = round(surface / 10.764)
+                item_db["surface"] = surface
+            elif not item_db.get("surface") and KEYWORDS["surface(ft2)"] in detail:
+                item_db["surface"] = round(int(detail[:4].replace(" ", "")) / 10.764)
+            elif not item_db.get("bedrooms") and KEYWORDS["bedrooms"] in detail:
+                item_db["bedrooms"] = int(detail[0])
+            elif not item_db.get("furnished") and detail in KEYWORDS["furnished"]:
+                item_db["furnished"] = detail
+            elif not item_db.get("description") and detail == KEYWORDS["description"]:
+                item_db["description"] = details[i + 1].text.replace("Voir moins", "")
 
-        img = fb.driver.find_element_by_xpath(
-            "//img[@referrerpolicy='origin-when-cross-origin']"
-        )
-        db["image"].append(img.get_attribute("src"))
+        item_db["image"] = set()
+        try:
+            img_cnt = -1
+            while len(item_db["image"]) != img_cnt:
+                img_cnt = len(item_db["image"])
+                img = self.driver.find_element_by_xpath(
+                    "//img[@referrerpolicy='origin-when-cross-origin']"
+                )
+                item_db["image"].add(img.get_attribute("src"))
+                next_button = self.driver.find_element_by_xpath(
+                    "//div[@aria-label='Voir l’image suivante']"
+                )
+                next_button.click()
+                sleep(random.uniform(0.6, 1))
+        except:
+            pass
 
-        for key, value in db.items():
+        for key, value in item_db.items():
             if key != "image":
-                print(key + ":", value[-1])
-            else:
-                response = requests.get(db["image"][-1])
-                Image.open(BytesIO(response.content)).resize((400, 400)).show()
+                print(key + ":", value)
+        print("\n  " + "=" * 40 + "\n  " + "=" * 40 + "\n")
 
-        return db
-        # try:
-        #     previous_and_next_buttons = self.driver.find_elements_by_xpath(
-        #         "//i[contains(@class, '_3ffr')]"
-        #     )
-        #     next_image_button = previous_and_next_buttons[1]
-        #     while next_image_button.is_displayed():
-        #         next_image_button.click()
-        #         image_element = self.driver.find_element_by_xpath(
-        #             '//img[contains(@class, "_5m")]'
-        #         )
-        #         sleep(1)
-        #         if image_element.get_attribute("src") in images:
-        #             break
-        #         else:
-        #             images.append(image_element.get_attribute("src"))
-        # except:
-        #     pass
+        return item_db
+
+    def scrape_items_details(self):
+        if len(self.items_links):
+            for item_url in self.items_links[:5]:
+                item_db = self.scrape_item_details(item_url)
+                for key in self.db:
+                    self.db[key].append(item_db.get(key, ""))
+        else:
+            print("You should first scrape items links")
+        return self.db
 
 
 if __name__ == "__main__":
     fb = Facebook()
     fb.log_in()
-    fb.scrape_item_links()
-    fb.scrape_item_details(item_url=fb.item_links[0])
-    fb.quit()
+    fb.scrape_items_links(scroll=0)
+    fb.scrape_items_details()
+    for key, value in fb.db.items():
+        if key not in ("image", "description"):
+            print(f"-> {key}: ")
+            for v in value:
+                print(v)
+            print()
+    # fb.quit()
 
---
+++ flatfindr/utils.py
@@ -0,0 +1,11 @@
+KEYWORDS = {
+    "published": "Mis en vente il y a ",
+    "price": "$ / mois",
+    "address": "Montréal, QC",
+    "surface(m2)": "mètres carrés",
+    "surface(ft2)": "pieds carrés",
+    "bedrooms": "chambres · ",
+    "furnished": ("Meublé", "Non meublé"),
+    "description": "Description",
+    "day": "jours",
+}
