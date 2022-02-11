fe73164b44c1c99edf6cde35726ebf5ded68864b
Alexandre Duverger <alexandre.duverger@protonmail.com>
2022-02-11 14:35:32 -0500

    tests for github CI dealing with secrets

--- tests/test_facebook.py
+++ tests/test_facebook.py
@@ -4,6 +4,8 @@ import io
 import os
 import unittest
 import unittest.mock
+import platform
+
 
 from flatfindr.facebook import Facebook
 
@@ -19,6 +21,11 @@ class TestLogIn(unittest.TestCase):
     @unittest.mock.patch("sys.stdout", new_callable=io.StringIO)
     def test_log_in(self, mock_stdout):
         fb = Facebook(headless=True, db_path=TEST_DB_PATH)
+        if platform.system() == "Linux":
+            # if test is ran from GitHub
+            # TODO: better way to check
+            fb.email = os.environ["FB_LOGIN"]
+            fb.password = os.environ["FB_PWD"]
         fb.log_in()
         self.assertEqual(mock_stdout.getvalue(), "")
         fb.quit_driver()
