1eaddd16ce837a5847e270c7773d5ba26aa11d09
Alexandre Duverger <alexandre.duverger@protonmail.com>
2022-02-11 12:42:15 -0500

    tests now create a temporary db

--- tests/test_facebook.py
+++ tests/test_facebook.py
@@ -1,6 +1,7 @@
 # pylint: disable-all
 
 import io
+import os
 import unittest
 import unittest.mock
 
@@ -8,11 +9,17 @@ from flatfindr.facebook import Facebook
 
 """ Test the Facebook Class """
 
+TEST_DB_PATH = os.path.join(
+    os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
+    os.path.join("raw_data", "test_db.json"),
+)
+
+fb = Facebook(headless=False, db_path=TEST_DB_PATH)
+
 
 class TestLogIn(unittest.TestCase):
     @unittest.mock.patch("sys.stdout", new_callable=io.StringIO)
     def test_log_in(self, mock_stdout):
-        fb = Facebook(headless=False)
         fb.log_in()
         self.assertEqual(mock_stdout.getvalue(), "")
         fb.quit_driver()
