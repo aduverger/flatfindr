c8583cf925d22e0677d848d9ab4c461b9a40771e
Alexandre Duverger <alexandre.duverger@protonmail.com>
2022-02-12 15:36:06 -0500

    loginstemplate back

---
+++ flatfindr/logins-template.py
@@ -0,0 +1,12 @@
+"""
+    Store here your logins and passwords for the different website you need to log in.
+    Currently, this is only mandatory for Facebook. All other suported website can be scraped without being loged in.
+    If you're working with a github and any other public online repositery, be sure to add this file to your .gitignore file.
+"""
+
+LOGINS = {
+    "facebook": {"id": r"<ID>", "pass": r"<PWD>"},
+    "telegram": r"<TOKEN>",
+}
+
+URL = {"facebook": "https://www.facebook.com"}
